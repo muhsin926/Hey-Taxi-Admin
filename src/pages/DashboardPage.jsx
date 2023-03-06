92a3cd16b4f99c4534697a803de20899f8a8f3d4
muhsin926 <muhsinmp926@gmail.com>
2023-03-06 17:18:15 +0530

    dashboard responsive

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -7,11 +7,11 @@ const DashboardPage = () => {
   return (
     <main>
       <Navbar />
-      <section className="min-h-screen py-10 bg-black grid grid-cols-12 text-white">
+      <section className="min-h-screen md:py-10 bg-black grid grid-cols-12 text-white">
         <aside className="hidden md:block col-span-2 ml-7  bg-zinc-800 h-3/4 w-full p-8 rounded-lg">
           <SideBar />
         </aside>
-        <section className="col-span-10 ml-12 w-11/12 bg-zinc-800  rounded-xl p-10">
+        <section className="col-span-12 md:col-span-10 md:ml-12 w-full md:w-11/12 bg-zinc-800  rounded-xl p-10">
           <Dashboard />
         </section>
       </section>
